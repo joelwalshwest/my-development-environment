a4222645920034c1d049bcde2fe4ded71aff16ce
Joel Walsh-West <joelwalshwest@gmail.com>
2024-08-09 14:41:21 -0400

    Adds global search option

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -16,7 +16,13 @@ telescope = require("telescope")
 
 vim.keymap.set("n", "<leader>e", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
 vim.keymap.set("n", "<leader>o", builtin.find_files, {})
-vim.keymap.set("n", "<leader>O", builtin.find_files, {})
+vim.keymap.set(
+    "n",
+    "<leader>O",
+    function()
+        builtin.find_files({cwd = "/", hidden = true})
+    end
+)
 vim.keymap.set("n", "<leader>h", builtin.git_files, {})
 vim.keymap.set(
     "n",
