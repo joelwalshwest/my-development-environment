9d4387f1e51200afb00831c73089448555b02d14
Joel Walsh-West <joelwalshwest@gmail.com>
2024-08-17 12:26:12 -0400

    Adds better Floaterm keybindings

--- nvim/lua/joelwalshwest/floatterm.lua
+++ nvim/lua/joelwalshwest/floatterm.lua
@@ -1,10 +1,14 @@
-vim.cmd([[
+vim.cmd(
+    [[
   let g:floaterm_width = 0.95
   let g:floaterm_height = 0.95
   let g:floaterm_autoinsert = v:true
   nnoremap <leader>t :FloatermToggle<CR>
   nnoremap <leader>T :FloatermNew<CR>
-  nnoremap <leader>; :FloatermNext<CR>
-  nnoremap <leader>: :FloatermPrev<CR>
+  tnoremap <c-k> <C-\><C-n>:FloatermNext<CR>
+  tnoremap <c-j> <C-\><C-n>:FloatermPrev<CR>
+  tnoremap <c-x> <C-\><C-n>:FloatermKill<CR>
   tnoremap <Esc> <C-\><C-n>
-]])
+  "tunmap <Space>t
+]]
+)
