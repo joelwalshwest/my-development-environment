003527116d64c9e668cf2a2b5e387ba875944d09
Joel Walsh-West <joelwalshwest@gmail.com>
2024-11-27 10:52:47 -0500

    Adds better codecompanion key bindings

--- nvim/lua/joelwalshwest/code-companion.lua
+++ nvim/lua/joelwalshwest/code-companion.lua
@@ -1,3 +1,3 @@
 vim.keymap.set("n", "<leader>c", "<cmd>CodeCompanionChat Toggle<CR>")
-vim.keymap.set("n", "<leader>i", "<cmd>CodeCompanion<CR>/buffer ")
-vim.keymap.set("v", "<leader>i", "<cmd>CodeCompanion<CR>/buffer ")
+vim.keymap.set("n", "<leader>C", "<cmd>CodeCompanion<CR>/buffer ")
+vim.keymap.set("v", "<leader>C", "<cmd>CodeCompanion<CR>/buffer ")
