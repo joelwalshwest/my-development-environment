2947f6922f75fd06cc60b9ac3cf0d3d12591c545
= <joelwalshwest@gmail.com>
2024-12-26 15:14:29 -0500

    adds untracked files to telescope git command

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -16,7 +16,7 @@ telescope = require("telescope")
 
 vim.keymap.set("n", "<leader>e", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
 vim.keymap.set("n", "<leader>O", builtin.find_files, {})
-vim.keymap.set("n", "<leader>o", builtin.git_files, {})
+vim.keymap.set("n", "<leader>o", ":Telescope git_files show_untracked=true<CR>")
 vim.keymap.set(
     "n",
     "<leader>O",
