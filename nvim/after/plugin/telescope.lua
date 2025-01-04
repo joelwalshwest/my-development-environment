645fdd9f7008c5bfe3f3793934f3466c9a7bbd7c
= <joelwalshwest@gmail.com>
2025-01-03 19:34:21 -0500

    Removes git mapping that does the same thing
    Big win is it doesn't require being in a repo to work

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -16,7 +16,7 @@ telescope = require("telescope")
 
 vim.keymap.set("n", "<leader>e", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
 vim.keymap.set("n", "<leader>O", builtin.find_files, {})
-vim.keymap.set("n", "<leader>o", ":Telescope git_files show_untracked=true<CR>")
+vim.keymap.set("n", "<leader>o", ":Telescope find_files<CR>")
 vim.keymap.set(
     "n",
     "<leader>O",
